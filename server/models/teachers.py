9419c918a0d358724f70e691f34e954e7923ef82
malci <malki@gimel.com>
2026-05-24 18:07:14 +0300

    Add models and dto

--- server/models/teachers.py
+++ server/models/teachers.py
@@ -1,10 +1,7 @@
-from sqlalchemy import (
-    Column, Integer, String,
-    Boolean
-)
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-from models.base import Base
+Base = declarative_base()
 
 
 class Teacher(Base):
