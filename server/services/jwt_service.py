1f4326fe9ef543a33c3506accbfed6f108f5c497
malci <malki@gimel.com>
2026-06-22 09:32:49 +0300

    Add auth and check answer services

---
+++ server/services/jwt_service.py
@@ -0,0 +1,19 @@
+import jwt
+import datetime
+from config import Config
+
+def create_token(user: dict):
+    payload = {
+        "user_id": user["id"],
+        "role": user["role"],
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+    }
+
+    return jwt.encode(payload, Config.SECRET_KEY, algorithm="HS256")
+
+def decode_token(token: str):
+    return jwt.decode(
+        token,
+        Config.SECRET_KEY,
+        algorithms=["HS256"]
+    )
